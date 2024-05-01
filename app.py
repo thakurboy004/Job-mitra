3f86553b18164f282cb1354ab2d3a63c88d855c6
thakurboy004 <vicky986847@gmail.com>
2024-05-01 21:48:33 +0530

    Added some more files

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='templates')
 
-@app.route("/")
-def first_page():
+@app.route('/')
+def index():
     return render_template("home.html")
+
+@app.route('/jobs')
+def jobs():
+    return render_template("jobs.html", page_title="Jobs")
+
+@app.route('/about')
+def about():
+    return render_template("about.html", page_title="About Us")
