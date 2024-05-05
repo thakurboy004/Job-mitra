cbec66e0bbfbda53d8cc1ce16ee209cdac1b5cae
thakurboy004 <vicky986847@gmail.com>
2024-05-05 21:50:02 +0530

    Adding Final Touch

--- app.py
+++ app.py
@@ -1,16 +1,34 @@
 from flask import Flask, render_template, request
-# from database import get_job_from_db, save_application_into_db
+from database import get_job_from_db, save_application_into_db, load_jobs_from_db
 
 app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def index():
-    return render_template("home.html")
+    jobs = load_jobs_from_db()
+    return render_template("home.html", jobs=jobs)
 
 @app.route('/jobs')
 def jobs():
-    return render_template("jobs.html", page_title="Jobs")
+    jobs = load_jobs_from_db()
+    return render_template("jobs.html", page_title="Jobs", jobs=jobs)
 
 @app.route('/about')
 def about():
     return render_template("about.html", page_title="About Us")
+
+@app.route('/job/<id>')
+def load_job(id):
+    job = get_job_from_db(id)
+    return render_template("job.html", page_title=job[1], job=job)
+
+@app.route('/apply/<id>', methods=['POST'])
+def apply(id):
+    job = get_job_from_db(id)
+    applicant_name = request.form['full_name']
+    gender = request.form['gender']
+    explanation = request.form['additional_info']
+    resume = request.form['resume_url']
+    message = save_application_into_db(id, applicant_name, gender, explanation, resume)
+    return render_template('job.html', page_title=job[1], job=job, message=message)
+
--- database.py
+++ database.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine, text
 import os
 
-engine = create_engine(os.getenv('DATABASE_URL'))
+engine = create_engine(os.getenv('DATABASE_URL'), echo=True)
 
-# getting jobs from database
+# getting job from database
 def get_job_from_db(id):
     with engine.connect() as connection:
         result = connection.execute(text("SELECT * FROM jobs WHERE id = :id"), {'id': id})
@@ -13,6 +13,18 @@ def get_job_from_db(id):
 # Storing the application into the database
 def save_application_into_db(id, applicant_name, gender, explanation, resume):
     with engine.connect() as connection:
-        connection.execute(text("INSERT INTO applications (id, applicant_name, gender, explanation, resume) VALUES (:id, :applicant_name, :gender, :explanation, :resume)"), {'id': id, 'applicant_name': applicant_name, 'gender':gender, 'explanation': explanation, 'resume': resume})
+        connection.execute(text("INSERT INTO applications (`id`, `applicant_name`, `gender`, `explanation`, `resume_url`) VALUES (:id, :applicant_name, :gender, :explanation, :resume)"), {'id': id, 'applicant_name': applicant_name, 'gender':gender, 'explanation': explanation, 'resume': resume})
+        connection.commit()
     return "Your application is under process. We will get back to you soon."
 
+
+
+def load_jobs_from_db():
+    # Assuming you have already created the engine object with the correct database URL
+    # engine = create_engine('sqlite:///example.db', echo=True)  # Update with your database URL
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM jobs"))
+        jobs = []
+        for row in result.fetchall():
+            jobs.append(row)  # Append the row directly, no need to convert to dictionary
+        return jobs
